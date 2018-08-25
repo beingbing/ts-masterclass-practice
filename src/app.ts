beb548b3752d401d882a1f35e20deee501442f97
beingbing <shaikhsamartaj@gmail.com>
2018-08-25 22:35:12 +0530

    04: typing "this" and "noImplicitThis"

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,8 @@
-class MyClass {
-    myMethod() {
-        // const foo = 123;
-        console.log('1', this);
-        // setTimeout(function() {
-        //     console.log(this); // setTimeout is called global, hence the result.
-        //     // console.log(foo); // it worked because foo is available in scope chain
-        // }, 0);
-        setTimeout(() => {
-            console.log('2', this);
-        }, 0); // arrow function doesn't creates a new scope, hence it uses its parents scope
-    }
-}
-// although setTimeout creates a brand new scope, if something is not available in
-// its scope then it goes to look it in its parent scope.
-// what we are using is called lexical scope, so the variable 'foo' that we are
-// using is available in the lexical scope.
+const elem = document.querySelector('.click');
 
-const myInstance = new MyClass();
-myInstance.myMethod();
+function handleClick(this: HTMLAnchorElement, event: Event) {
+    event.preventDefault();
+    console.log(this.className);
+}
+
+elem.addEventListener('click', handleClick, false);
