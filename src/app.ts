9ec8c1ddb92580b90cdf612c922acae73179a212
beingbing <shaikhsamartaj@gmail.com>
2018-08-25 23:04:33 +0530

    07: "keyof", generics and lookup types

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,12 @@ type Person = typeof person;
 type PersonKeys = keyof Person;
 type PersonTypes = Person[PersonKeys];
 
+function getProperty(obj: Person, key: string) {
+    return obj[key];
+}
+
+const personName = getProperty(person, 'name');
+
 const anotherPerson: Person = {
     name: 'sana',
     age: 21
