bd1676c71ab7ed856a90bed806b5be11c950510e
beingbing <shaikhsamartaj@gmail.com>
2018-08-26 17:08:49 +0530

    18: discriminated (tagged) unions

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,19 @@ interface Order {
 }
 
 interface Stripe {
+    type: 'stripe';
     card: string;
     cvc: number;
 }
 
 interface PayPal {
+    type: 'paypal';
     email: string;
 }
 
 type CheckoutCard = Order & Stripe;
 type CheckoutPayPal = Order & PayPal;
-type CheckoutABC = Order & { name: string };
+// type CheckoutABC = Order & { name: string };
 
 const order: Order = {
     id: 'fd43fg',
@@ -25,13 +27,26 @@ const order: Order = {
 
 const orderCard: CheckoutCard = {
     ...order,
+    type: 'stripe',
     card: '1000 2000 3000 4000',
     cvc: 435
 }
 
 const orderPayPal: CheckoutPayPal = {
     ...order,
+    type: 'paypal',
     email: 'abd@sdf.com'
 };
 
 const assigned = Object.assign({}, order, orderCard);
+
+type Payload = CheckoutCard | CheckoutPayPal;
+
+function checkout(payload: Payload) {
+    if (payload.type === 'stripe') {
+        console.log(payload.card, payload.cvc)
+    }
+    if (payload.type === 'paypal') {
+        console.log(payload.email);
+    }
+}
