0d8d7da9e4f2ebfac2a0ac7f342aac2979acc2cf
beingbing <shaikhsamartaj@gmail.com>
2018-08-26 17:32:51 +0530

    20: Interface and classes

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,32 @@
-interface Item {
-    name: string;
+// class is more of a blueprint that object shares (same info, properties and methods)
+// Interface is a group of related properties and methods that describe an object
+//
+// but neither provides the implementations nor initialization for objects but 
+// interface doesn't provide any implementation details nor it allows to initialize them
+
+// the use case for both depends on whether we just want to use them for type checking
+// or we want to provide implementation details
+
+// interface Artist {
+//     name: string;
+// }
+
+// new Artist('Samar'); // we can't do that with interface
+
+class ArtistCreator /*implements Artist*/ {
+    constructor(public name: string) {}
 }
 
-interface Artist extends Item {
-    songs: number;
+// function artistFactory({ name }: Artist) {
+//     return { id: 101, name }
+// }
+
+// function artistFactory({ name }: Artist) {
+//     return new ArtistCreator(name);
+// }
+
+function artistFactory({ name }: ArtistCreator) {
+    return new ArtistCreator(name);
 }
 
-interface Artist {
-    getSongs(): number;
-} // interface with same name get merged
-
-type Artist2 = { name: string } & Item; // we can't have two types with same name
-// type Artist2 = { name: string } & Item;
-
-const newArtist: Artist = {
-    songs: 4,
-    name: 'atif',
-    getSongs() {
-        return this.songs;
-    }
-}
+artistFactory({ name: 'Samar'});
