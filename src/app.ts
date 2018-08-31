019f97ef23c1ad5095af01a7475aca147642bb01
beingbing <shaikhsamartaj@gmail.com>
2018-08-31 08:13:34 +0530

    26: writing declaration files

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,3 @@
-const enum Sizes  {
-    Small = 'small',
-    Medium = 'medium',
-    Large = 'large'
-}
+import * as _ from 'lodash';
 
-// do const when you are not using enum much
-
-let selected: Sizes;
-
-function updateSize(size: Sizes): void {
-    selected = size;
-}
-
-updateSize(Sizes.Large);
-
-console.log(selected);
+_.chunk([1,2,3,4], 2); // [[1,2], [3,4]]
